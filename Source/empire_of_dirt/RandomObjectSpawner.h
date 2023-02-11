d9b10b7fe0b002dbacb08cbf33b2d85856414fc4
gero <gero@limbicnation.com>
2023-02-11 07:44:16 +0100

    update DefaultGame.ini -> IniKeyBlacklis -> IniKeyDenyList

---
+++ Source/empire_of_dirt/RandomObjectSpawner.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class EMPIRE_OF_DIRT_API RandomObjectSpawner
+{
+public:
+	RandomObjectSpawner();
+	~RandomObjectSpawner();
+};
