32da5356756fdbec2ac2fd2d03ca38a950fc9262
gero <gero@limbicnation.com>
2023-02-13 14:32:27 +0100

    Add random spawner

---
+++ Source/empire_of_dirt/Components/SpawnBox.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/SpawnBox.h"
+
+// Sets default values
+ASpawnBox::ASpawnBox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Setup members
+	SpawnBox = CreateDefaultSubobject<UBoxComponent>(TEXT("SpawnBox"));
+	RootComponent = SpawnBox;
+
+}
+
+// Called when the game starts or when spawned
+void ASpawnBox::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// schedule first spawn
+	if (ShouldSpawn)
+	{
+		ScheduleActorSpawn();
+	}
+	
+}
+
+void ASpawnBox::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	// Remove all timers associated with this objects instance
+	GetWorld()->GetTimerManager().ClearAllTimersForObject(this);
+}
+
+bool ASpawnBox::SpawnActor()
+{
+	bool SpawnedActor = false;
+	if (ActorClassToBeSpawned)
+	{
+		// Calculate the extends of the box
+		FBoxSphereBounds BoxBounds = SpawnBox->CalcBounds(GetActorTransform());
+
+		// Compute random position within the box bounds
+		FVector SpawnLocation = BoxBounds.Origin;
+		SpawnLocation.X += -BoxBounds.BoxExtent.X + 2 * BoxBounds.BoxExtent.X * FMath::FRand();
+		SpawnLocation.Y += -BoxBounds.BoxExtent.Y + 2 * BoxBounds.BoxExtent.Y * FMath::FRand();
+		SpawnLocation.Z += -BoxBounds.BoxExtent.Z + 2 * BoxBounds.BoxExtent.Z * FMath::FRand();
+
+		// Spawn the actor
+		GetWorld()->SpawnActor(ActorClassToBeSpawned, &SpawnLocation);
+
+	}
+
+	return SpawnedActor;
+}
+
+void ASpawnBox::ScheduleActorSpawn()
+{
+	// 1. Compute time offset to spawn
+	float DeltaToNextSpawn = AvgSpawnTime + (-RandomSpawnTimeOffset + 2 * RandomSpawnTimeOffset * FMath::FRand());
+
+	// 2. Schedule spawning
+	GetWorld()->GetTimerManager().SetTimer(SpawnTimerHandle, this, &ASpawnBox::SpawnActorScheduled, DeltaToNextSpawn, false);
+}
+
+void ASpawnBox::SpawnActorScheduled()
+{
+	// Spawn and Reschedule if required
+	if (SpawnActor())
+	{
+		if (ShouldSpawn)
+		{
+			ScheduleActorSpawn();
+		}
+	}
+	else
+	{
+		// TODO... log error
+	}
+}
+
---
+++ Source/empire_of_dirt/Components/SpawnBox.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "SpawnBox.generated.h"
+
+UCLASS()
+class EMPIRE_OF_DIRT_API ASpawnBox : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpawnBox();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	// Called when the actor stops playing
+	void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:	
+
+	// will spawn an actor of the specified class
+	UFUNCTION(BlueprintCallable)
+	bool SpawnActor();
+
+	// will schedule an actor spawn
+	void ScheduleActorSpawn();
+private:
+	UFUNCTION()
+	void SpawnActorScheduled();
+
+public:
+		// Actor class to spawn
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AActor> ActorClassToBeSpawned;
+
+	// Average time between spawns (without random)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float AvgSpawnTime = 5.f;
+	// Random +/- offset of the spawn time
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RandomSpawnTimeOffset = 1.f;
+
+private:
+	// Box in which we spawn the random Actors
+	UPROPERTY(EditDefaultsOnly)
+	UBoxComponent* SpawnBox;
+
+	UPROPERTY(EditAnywhere)
+	bool ShouldSpawn = true;
+
+	// Helper for timing
+	FTimerHandle SpawnTimerHandle;
+
+
+};
